9d76601f988ba655f2d7f0e3006f8d89b5310c05
Tom Wiesing <tkw01536@gmail.com>
2021-06-18 12:21:00 -0400

    Add legal script before pageload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import '../styles/latex.css'
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script async strategy="beforeInteractive" src="https://inform.everyone.wtf/legal.min.js?float" data-site-id="95fff7db-30c8-4480-ae71-58a1b20cd971" />
+    <Component {...pageProps} />
+  </>
 }
