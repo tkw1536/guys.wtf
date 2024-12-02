8a34ed8f29eee4a0f94250fa2e92e2374a01a389
GitHub Action <tkw1536@users.noreply.github.com>
2024-12-02 08:37:38 +0000

    7c55706a6f738fa0c72be30986279bdc9acfe6b5

---
+++ _next/static/chunks/pages/tabs/[tab]-1f55e89644f76b4e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[242],{9176:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/tabs/[tab]",function(){return u(1676)}])}},function(n){n.O(0,[676,774,888,179],(function(){return _=9176,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
